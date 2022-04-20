8b0d190c75c5dc6ee94517eeca2a30e37f4c3783
Nilso97 <nilsojunior90@gmail.com>
2022-04-20 17:30:44 -0300

    second commit

---
+++ promises.js
@@ -0,0 +1,22 @@
+// Promises
+const loginUserPromise = (email) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const error = false;
+            if (error) reject(new Error('error in login!'));
+            console.log('user logged!');
+            resolve({
+                email
+            });
+        }, 2000);
+    });
+}
+loginUserPromise('nilsojunior90@gmail.com').then((user) => {
+    console.log({
+        user
+    });
+}).catch((error) => {
+    console.log({
+        error
+    });
+});
